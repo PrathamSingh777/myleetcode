db5013b4c68aabfb887f7ea43f01645cf7a72f58
Pratham Singh <156084619+PrathamSingh777@users.noreply.github.com>
2026-07-04 21:58:01 +0530

    Add solution for Valid Parentheses

---
+++ solutions/easy/Valid Parentheses.java
@@ -0,0 +1,29 @@
+// Title: Valid Parentheses
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/valid-parentheses/
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+
+            Character ch = s.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } else {
+                if (st.isEmpty()) {
+                    return false;
+                } else if ((ch == ')' && st.peek() == '(') || (ch == '}' && st.peek() == '{')
+                        || (ch == ']' && st.peek() == '[')) {
+                    st.pop();
+                } else {
+                    return false; // mismatch
+                }
+            }
+
+        }
+
+        return st.isEmpty();
+    }
