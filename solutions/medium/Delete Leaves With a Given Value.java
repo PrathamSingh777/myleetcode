c61104046524eb5956f7221086b447663084ada8
Pratham Singh <156084619+PrathamSingh777@users.noreply.github.com>
2026-07-24 14:17:40 +0530

    Add solution for Delete Leaves With a Given Value

---
+++ solutions/medium/Delete Leaves With a Given Value.java
@@ -0,0 +1,23 @@
+// Title: Delete Leaves With a Given Value
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/delete-leaves-with-a-given-value/
+
+class Solution {
+    public TreeNode removeLeafNodes(TreeNode root, int target) {
+        
+if(root==null ){
+    return null;
+}
+
+root.left = removeLeafNodes(root.left, target);
+root.right = removeLeafNodes(root.right, target);
+
+if (root.left == null &&
+    root.right == null &&
+    root.val == target) {
+    return null;
+}
+
+return root;}}
+/**
