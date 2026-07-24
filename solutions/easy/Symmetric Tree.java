5008d218338b9c73cf6fd9dcca8c6514841f8dce
Pratham Singh <156084619+PrathamSingh777@users.noreply.github.com>
2026-07-24 10:14:43 +0530

    Add solution for Symmetric Tree

---
+++ solutions/easy/Symmetric Tree.java
@@ -0,0 +1,29 @@
+// Title: Symmetric Tree
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/symmetric-tree/
+
+        invertTree(rooti.right);
+
+        return rooti;
+    }
+
+    // <-- Moved inside Solution class
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) &&
+               isSameTree(p.right, q.right);
+    }
+}
