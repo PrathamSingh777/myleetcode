1e5e5b5b0b8310e7375685d0dd2420ab5167fffb
Pratham Singh <156084619+PrathamSingh777@users.noreply.github.com>
2026-08-14 22:56:39 +0530

    Add solution for Validate Binary Search Tree

---
+++ solutions/medium/Validate Binary Search Tree.java
@@ -0,0 +1,28 @@
+// Title: Validate Binary Search Tree
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/validate-binary-search-tree/
+
+    }
+
+private void dfs(TreeNode root){
+    if(root==null){
+    Boolean valid =true;
+    public boolean isValidBST(TreeNode root) {
+        if(root==null){
+            return true;
+        }
+        dfs(root);
+        return valid;        
+class Solution {
+/**
+            return ;
+        }
+dfs(root.left);
+if(root.val<=prev){
+valid=false;
+return ;
+}
+else{
+    prev=root.val;
+    long prev = Long.MIN_VALUE;
