147a9196a2c7d968cea28ef2975d09059c0a016f
Pratham Singh <156084619+PrathamSingh777@users.noreply.github.com>
2026-08-20 12:46:00 +0530

    Add solution for Minimum Total Price After Applying Discounts

---
+++ solutions/medium/Minimum Total Price After Applying Discounts.java
@@ -0,0 +1,5 @@
+// Title: Minimum Total Price After Applying Discounts
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/minimum-total-price-after-applying-discounts/
+
