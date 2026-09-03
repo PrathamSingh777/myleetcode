45c3a4d93eaa76e25a338207ae4f7d507f367bf7
Pratham Singh <156084619+PrathamSingh777@users.noreply.github.com>
2026-09-03 15:44:46 +0530

    Add solution for Delete Node in a BST

---
+++ solutions/medium/Delete Node in a BST.java
@@ -0,0 +1,31 @@
+// Title: Delete Node in a BST
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/delete-node-in-a-bst/
+
+
+            // Case 2: only right child
+            if (root.left == null) {
+                return root.right;
+            }
+            }
+                return null;
+            if (root.left == null && root.right == null) {
+            // Case 1: no child
+
+        else {
+
+        }
+            root.right = deleteNode(root.right, key);
+
+        else if (key > root.val) {
+        }
+            root.left = deleteNode(root.left, key);
+
+        if (key < root.val) {
+        }
+            return null;
+        if (root == null) {
+
+    public TreeNode deleteNode(TreeNode root, int key) {
+class Solution {
