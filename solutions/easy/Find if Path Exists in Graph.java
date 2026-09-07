682a1aa2a2d10bdc47c5cd7fa7f1b800bbd83ec5
Pratham Singh <156084619+PrathamSingh777@users.noreply.github.com>
2026-09-07 21:59:46 +0530

    Add solution for Find if Path Exists in Graph

---
+++ solutions/easy/Find if Path Exists in Graph.java
@@ -0,0 +1,29 @@
+// Title: Find if Path Exists in Graph
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/find-if-path-exists-in-graph/
+
+
+        boolean[] visited = new boolean[n];
+        return trav(source, destination, visited);
+    }
+
+   private boolean trav(int source, int destination, boolean[] visited){
+
+        if (source == destination) {
+        }
+            map.get(a).add(b);
+            map.get(b).add(a);
+            int b = edges[j][1];
+        for (int j = 0; j < edges.length; j++){
+            int a = edges[j][0];
+
+            map.put(i, new ArrayList<>());
+        }
+
+        for (int i = 0; i < n; i++) {
+
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+
+    HashMap<Integer, List<Integer>> map = new HashMap<>();
+class Solution {
