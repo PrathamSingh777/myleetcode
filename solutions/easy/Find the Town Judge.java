41ccd9cd0154f1335e338eab1ab987a629ebc600
Pratham Singh <156084619+PrathamSingh777@users.noreply.github.com>
2026-09-08 23:14:03 +0530

    Add solution for Find the Town Judge

---
+++ solutions/easy/Find the Town Judge.java
@@ -0,0 +1,25 @@
+// Title: Find the Town Judge
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/find-the-town-judge/
+
+
+for(int i=0; i<trust.length; i++){
+    int a = trust[i][0];
+}
+      int b = trust[i][1];
+indegree[b]++;
+
+outdegree[a]++;
+
+for(int j=1;j<=n;j++){
+    if(indegree[j]==n-1 && outdegree[j]==0){
+}
+        return j;
+    }
+int[] outdegree = new int [n+1]; 
+        
+int[] indegree = new int [n+1]; 
+
+class Solution {
+    public int findJudge(int n, int[][] trust) {
