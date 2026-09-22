df27051c57c1b240555fa1235573f21aa7f3a59d
Pratham Singh <156084619+PrathamSingh777@users.noreply.github.com>
2026-09-22 19:22:40 +0530

    Add solution for Course Schedule

---
+++ solutions/medium/Course Schedule.java
@@ -0,0 +1,28 @@
+// Title: Course Schedule
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/course-schedule/
+
+            }
+            if (indegree[i] == 0) {
+                queue.offer(i);
+ Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < numCourses; i++) {
+        }
+            indegree[course]++;
+
+            adj.get(pre).add(course);
+            int course = prerequisite[0];
+            int pre = prerequisite[1];
+    adj.add(new ArrayList<>());
+}
+        int[] indegree = new int[numCourses];
+
+ for (int[] prerequisite : prerequisites) {
+
+        List<List<Integer>>adj = new ArrayList<>();
+
+for(int i=0; i<numCourses; i++){
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
