a3f6645c2ba3596297d96f6c3b6e9f5ef4b5da3b
Pratham Singh <156084619+PrathamSingh777@users.noreply.github.com>
2026-09-24 22:57:52 +0530

    Add solution for Verifying an Alien Dictionary

---
+++ solutions/easy/Verifying an Alien Dictionary.java
@@ -0,0 +1,28 @@
+// Title: Verifying an Alien Dictionary
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/verifying-an-alien-dictionary/
+
+    break;
+}
+
+for(int k= 0 ; k<p; k++){
+if(map.get(st1.charAt(k))<map.get(st2.charAt(k))){
+ int len = Math.min(p, t);
+
+int p = st1.length();
+int t = st2.length();
+
+for(int i=0; i<words.length-1;i++){
+String st1 = words[i];
+String st2 = words[i+1];
+for(int i =0; i<ln; i++){
+    map.put(order.charAt(i), val);
+    val++;
+}
+
+class Solution {
+    public boolean isAlienSorted(String[] words, String order) {
+        int ln = order.length();
+        int val = 0;
+HashMap<Character, Integer> map = new HashMap<>(ln);
